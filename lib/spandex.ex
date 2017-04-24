b947a879579f77ae602b0c529035c68577480bfd
zachdaniel <zachary.s.daniel@gmail.com>
2017-04-24 16:16:24 -0400

    added service creation on initialization.  This may not be the best way to achieve this

--- lib/spandex.ex
+++ lib/spandex.ex
@@ -3,16 +3,20 @@ defmodule Spandex do
   Documentation for Spandex.
   """
 
-  @doc """
-  Hello world.
+  def create_services() do
+    services = Application.get_env(:spandex, :services, [])
+    application_name = Application.get_env(:spandex, :application_name)
+    host = Application.get_env(:spandex, :host)
+    port = Application.get_env(:spandex, :port)
+    for {service_name, type} <- services do
+      body = %{
+        service_name => %{
+          "app" => application_name,
+          "type" => type
+        }
+      }
 
-  ## Examples
-
-      iex> Spandex.hello
-      :world
-
-  """
-  def hello do
-    :world
+      HTTPoison.put("#{host}:#{port}/v0.3/services", Poison.encode!(body) |> IO.inspect, [{"Content-Type", "application/json"}]) |> IO.inspect
+    end
   end
 end
