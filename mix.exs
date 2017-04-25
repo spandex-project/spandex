7087a7c0098f60b57c2df68e18a32516b3957e9d
zachdaniel <zachary.s.daniel@gmail.com>
2017-04-25 04:17:01 -0500

    bumped version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Spandex.Mixfile do
 
   def project do
     [app: :spandex,
-     version: "0.0.12",
+     version: "0.0.13",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
