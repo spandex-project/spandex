1eab747dab4f0052d887c75cd514f1897581319c
zachdaniel <zachary.s.daniel@gmail.com>
2017-04-28 09:34:52 -0500

    added the ability to ignore routes

--- lib/spandex/plug/start_trace.ex
+++ lib/spandex/plug/start_trace.ex
@@ -6,17 +6,35 @@ defmodule Spandex.Plug.StartTrace do
 
   @spec call(Plug.Conn.t, Keyword.t) :: Plug.Conn.t
   def call(conn, opts) do
-    unless Application.get_env(:spandex, :disabled?) do
-      ignored_methods = Application.get_env(:spandex, :ignored_methods) || []
-      unless conn.method in ignored_methods do
-        case Spandex.Trace.start(opts) do
-          {:ok, pid} ->
-            :ets.insert(:spandex_trace, {self(), pid})
-          _ -> :error
-        end
+    unless ignoring_request?(conn) do
+      case Spandex.Trace.start(opts) do
+        {:ok, pid} ->
+          :ets.insert(:spandex_trace, {self(), pid})
+        _ -> :error
       end
     end
 
     conn
   end
+
+  defp ignoring_request?(conn) do
+    disabled?() || ignored_method?(conn) || ignored_route?(conn)
+  end
+
+  defp disabled?() do
+    !!Application.get_env(:spandex, :disabled?)
+  end
+
+  defp ignored_method?(conn) do
+    ignored_methods = Application.get_env(:spandex, :ignored_methods, [])
+    conn.method in ignored_methods
+  end
+
+  defp ignored_route?(conn) do
+    ignored_routes = Application.get_env(:spandex, :ignored_routes, [])
+    Enum.any?(ignored_routes, fn ignored_route ->
+      String.match?(conn.request_path, ignored_route)
+    end)
+  end
+
 end
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Spandex.Mixfile do
 
   def project do
     [app: :spandex,
-     version: "0.1.5",
+     version: "0.1.6",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
