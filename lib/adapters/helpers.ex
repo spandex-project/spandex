a49963a86511480f7cc5150974c736081ea393ca
zachdaniel <zachary.s.daniel@gmail.com>
2017-06-19 10:27:07 -0500

    delegating to adapter to avoid issues when adapter is not configured

---
+++ lib/adapters/helpers.ex
@@ -0,0 +1,22 @@
+defmodule Spandex.Adapters.Helpers do
+  @moduledoc """
+  Helper functions and macros for adapters.
+  """
+
+  defmacro delegate_to_adapter(name, args) do
+    quote do
+      def unquote(name)(unquote_splicing(args)) do
+        adapter = Confex.get(:spandex, :adapter)
+
+        if adapter do
+          apply(adapter, unquote(name), unquote(args))
+        else
+          {:error, :no_adapter_configured}
+        end
+      rescue
+        exception ->
+          {:error, exception}
+      end
+    end
+  end
+end
