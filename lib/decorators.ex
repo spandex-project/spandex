1e9af8a65580efbf21a6a58cd05e0bdfd34e0b48
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2018-09-30 11:37:10 +0100

    Improve moduledoc for Spandex.Decorators
    As suggested by @zachdaniel :raised_hands:

--- lib/decorators.ex
+++ lib/decorators.ex
@@ -2,19 +2,25 @@ if Code.ensure_loaded?(Decorator.Define) do
   defmodule Spandex.Decorators do
     @moduledoc """
 
-    defmodule Foo do
-      use Spandex.Decorators
+    Span function decorators take an optional argument which is the attributes to update the span with. One of those attributes can be the `:tracer` in case you want to override the default tracer (e.g., in case you want to use multiple tracers).
 
-      @decorate trace()
-      def bar(a) do
-        a * 2
-      end
+    IMPORTANT If you define multiple clauses for a function, you'll have to decorate all of the ones you want to span.
 
-      @decorate trace(service: "ecto", type: "sql")
-      def databaz(a) do
-        a * 3
-      end
-    end
+    Note: Decorators don't magically do everything. It often makes a lot of sense to use `Tracer.update_span` from within your function to add details that are only available inside that same function.
+
+        defmodule Foo do
+          use Spandex.Decorators
+
+          @decorate trace()
+          def bar(a) do
+            a * 2
+          end
+
+          @decorate trace(service: "ecto", type: "sql")
+          def databaz(a) do
+            a * 3
+          end
+        end
     """
 
     @tracer Application.get_env(:spandex, :decorators)[:tracer]
