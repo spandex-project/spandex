f17ae7467cde3f4df4789812bf4b4eb843164b75
Jake Morrison <jake.morrison@therealreal.com>
2022-08-23 14:44:21 -0500

    Add :file and :line to test logs

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,7 @@ config :logger, :console,
   level: :debug,
   colors: [enabled: false],
   format: "$time $metadata[$level] $message\n",
-  metadata: [:trace_id, :span_id]
+  metadata: [:trace_id, :span_id, :file, :line]
 
 config :spandex, :decorators, tracer: Spandex.Test.Support.Tracer
 
