8fbd0ec24d8861331d8e485587c67ee55163b87a
Zach Daniel <zach@zachdaniel.dev>
2023-02-01 18:17:05 -0500

    improvement: configurable default priority

--- lib/spandex.ex
+++ lib/spandex.ex
@@ -580,7 +580,13 @@ defmodule Spandex do
         service_version: to_string(span.service_version)
       )
 
-      trace = %Trace{spans: [], stack: [span], id: trace_id}
+      trace = %Trace{
+        spans: [],
+        stack: [span],
+        id: trace_id,
+        priority: Application.get_env(:spandex, :default_priority, 1)
+      }
+
       strategy.put_trace(opts[:trace_key], trace)
     end
   end
